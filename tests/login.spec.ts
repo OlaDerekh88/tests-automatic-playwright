84e933800c18c7d3ea05f667215a27cb516204d7
Olga Derekh <olga.der88@gmail.com>
2026-05-07 22:46:12 +0300

    Set of tests and negative cases

---
+++ tests/login.spec.ts
@@ -0,0 +1,101 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('User login to Demobank', () => {
+
+  test('successful login with correct credentials', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('testerLO');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('12345678');
+    await page.getByTestId('login-button').click();
+    await page.getByTestId('user-name').click();
+
+    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+  });
+
+  test('unsuccessful login with to short username', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('tester');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('error-login-id').click();
+
+    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+  });
+  
+  test('unsuccessful login with to short password', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('testerLo');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('12345');
+    await page.getByTestId('password-input').blur();
+    
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { test, expect } from '@playwright/test';
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
