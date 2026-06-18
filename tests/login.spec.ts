79f21db8a27ab8708e953e567c89cf340ccda1b6
Olga Derekh <olga.der88@gmail.com>
2026-06-18 23:08:02 +0300

    Extension Playwright. Add login data. New payment test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
 
 test.describe('User login to Demobank', () => {
   test.beforeEach(async ({ page }) => {
@@ -7,8 +8,8 @@ test.describe('User login to Demobank', () => {
 
   test('successful login with correct credentials', async ({ page }) => {
     //Arrange
-    const userId = 'testerLO';
-    const userPassword = '12345678';
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
     const expectedUserName = 'Jan Demobankowy';
 
     //Act
@@ -37,7 +38,7 @@ test.describe('User login to Demobank', () => {
 
   test('unsuccessful login with too short password', async ({ page }) => {
     //Arrange
-    const userId = 'testerLO';
+    const userId = loginData.userId;
     const incorrectUserPassword = '12345';
     const errorLoginPassword = 'hasło ma min. 8 znaków';
 
