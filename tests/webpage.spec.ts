71a61aca1e1a8af3fc3b501cbbe0e59770b2f0ab
Olga Derekh <olga.der88@gmail.com>
2026-09-23 22:12:52 +0300

    chore: added tags

--- tests/webpage.spec.ts
+++ tests/webpage.spec.ts
@@ -4,7 +4,7 @@ import { HomePage } from '../src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify service main pages', () => {
-  test('home page title', async ({ page }) => {
+  test('home page title', { tag: ['@GAD-R01-01'] }, async ({ page }) => {
     // Arrange
     const homePage = new HomePage(page);
     // Act
@@ -15,7 +15,7 @@ test.describe('Verify service main pages', () => {
     expect(title).toContain('GAD');
   });
 
-  test('articles page tite', async ({ page }) => {
+  test('articles page title', { tag: ['@GAD-R01-02'] }, async ({ page }) => {
     //Arrange
     const articlesPage = new ArticlesPage(page);
 
@@ -27,7 +27,7 @@ test.describe('Verify service main pages', () => {
     expect(title).toContain('Articles');
   });
 
-  test('comments page tite', async ({ page }) => {
+  test('comments page title', { tag: ['@GAD-R01-02'] }, async ({ page }) => {
     //Arrange
     const commentsPage = new CommentsPage(page);
 
